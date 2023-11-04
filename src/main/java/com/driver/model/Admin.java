302bf2ead247245d0476a07d43964e1132f58c70
Alok290 <79790013+Alok290@users.noreply.github.com>
2023-11-04 19:46:18 +0530

    f5

--- src/main/java/com/driver/model/Admin.java
+++ src/main/java/com/driver/model/Admin.java
@@ -19,7 +19,7 @@ public class Admin {
 
 
     @OneToMany(mappedBy = "admin",cascade = CascadeType.ALL)
-    private List<ServiceProvider> serviceProviderList = new ArrayList<>();
+    private List<ServiceProvider> serviceProviders = new ArrayList<>();
 
 
     public Admin(){
@@ -29,7 +29,7 @@ public class Admin {
         this.id = id;
         this.username = username;
         this.password = password;
-        this.serviceProviderList = serviceProviders;
+        this.serviceProviders = serviceProviders;
     }
 
     public Integer getId() {
@@ -56,11 +56,11 @@ public class Admin {
         this.password = password;
     }
 
-    public List<ServiceProvider> getServiceProviderList() {
-        return serviceProviderList;
+    public List<ServiceProvider> getServiceProviders() {
+        return serviceProviders;
     }
 
-    public void setServiceProviderList(List<ServiceProvider> serviceProviderList) {
-        this.serviceProviderList = serviceProviderList;
+    public void setServiceProviders(List<ServiceProvider> serviceProviders) {
+        this.serviceProviders = serviceProviders;
     }
 }
